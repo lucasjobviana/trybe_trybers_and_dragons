fe2cd4a81524134ae5fd7300d43c2770f313731e
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-09-19 16:59:47 -0300

    :sparkles: feat: Adicionado classe Monster.

---
+++ src/Monster.ts
@@ -0,0 +1,36 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  public get lifePoints():number {
+    return this._lifePoints;
+  }
+
+  public get strength():number {
+    return this._strength;
+  }
+
+  attack(fighter: SimpleFighter): void {
+    fighter.receiveDamage(this._strength);
+  }
+ 
+  receiveDamage(attackPoints: number): number {
+    const damage = this._lifePoints - attackPoints;
+    if (this.lifePoints - damage <= 0) {
+      this._lifePoints = -1;
+    } else {
+      this._lifePoints -= damage;
+    }
+    
+    return this.lifePoints;
+  }
+
+  constructor() {
+    this._lifePoints = 3;
+    this._strength = 4;
+  }
+}
+
+export default Monster;
